d0ae89b324c36d7adb6ec258c818557cec45bae8
JuanRx19 <juanrx1904@gmail.com>
2024-05-19 15:00:09 -0500

    Avances 2.0

--- scenes/Menu/Main_Menu.gd
+++ scenes/Menu/Main_Menu.gd
@@ -4,10 +4,12 @@ extends Control
 @onready var play = $MarginContainer/HBoxContainer/VBoxContainer/Play as Button
 @onready var leave = $MarginContainer/HBoxContainer/VBoxContainer/Leave as Button
 @onready var start_level = preload("res://scenes/Menu/SelectLevel.tscn") as PackedScene
+@onready var audio_stream_player = $AudioStreamPlayer
 
 func _ready():
 	play.button_down.connect(on_start_pressed)
 	leave.button_down.connect(on_exit_pressed)
+	audio_stream_player.play()
 	
 func on_start_pressed() -> void:
 	get_tree().change_scene_to_packed(start_level)
